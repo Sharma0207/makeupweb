d13e1d3f2232825f26e22a6a1c16e05ebac209df
Balmukund Kumar <sharmabalmukun0207@gmail.com>
2025-05-10 20:08:00 +0530

    changed the postiton of computer canvas

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -21,8 +21,8 @@ const Computers = () => {
       <pointLight intensity={1} />
       <primitive
         object={computer.scene}
-        scale={0.75} // Keep scale normal for desktop
-        position={[0, -3.25, -1.5]}
+         scale={0.60} // Keep scale normal for desktop
+        position={[0, -4, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
