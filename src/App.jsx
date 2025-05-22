13fd40702b16e348d30a848dfddcdf63130911fc
Balmukund Kumar <sharmabalmukun0207@gmail.com>
2025-05-22 20:25:39 +0530

    added responsiveness4

--- src/App.jsx
+++ src/App.jsx
@@ -68,7 +68,7 @@ const App = () => {
           <Hero />
           <ImageGallery />
         </div>
-        <div className="mt-8 sm:mt-16 md:mt-0">
+        <div className="mt-8 sm:mt-32 md:mt-0">
           <About />
         </div>
         <Experience />
